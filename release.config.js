2a8447a464695736162ff09ee61971878ac28827
Robert Ing <ring@mparticle.com>
2021-11-03 10:14:16 -0400

    ci: refactor release.config.js to include generic dist folder instead of specific files within dist

--- release.config.js
+++ release.config.js
@@ -1,48 +1,45 @@
 module.exports = {
-    branches: ['master'],
-    tagFormat: 'v${version}',
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'angular',
-            },
-        ],
-        [
-            '@semantic-release/release-notes-generator',
-            {
-                preset: 'angular',
-            },
-        ],
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: 'CHANGELOG.md',
-            },
-        ],
-        ['@semantic-release/npm'],
-        [
-            '@semantic-release/exec',
-            {
-                prepareCmd: 'sh ./scripts/release.sh',
-            },
-        ],
-        [
-            '@semantic-release/github',
-            {
-                assets: [
-                    'dist/GoogleAdWordsEventForwarder.common.js',
-                    'dist/GoogleAdWordsEventForwarder.iife.js',
-                ],
-            },
-        ],
-        [
-            '@semantic-release/git',
-            {
-                assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
-                message:
-                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-            },
-        ],
+  branches: ["master"],
+  tagFormat: "v${version}",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+      },
     ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "angular",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    ["@semantic-release/npm"],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "sh ./scripts/release.sh",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: ["dist"],
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
 };
